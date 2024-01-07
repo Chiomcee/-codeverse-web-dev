0101028637124d2681108363cd7e1a50f6efe0c2
Chioma Igwe <cigwe072013@gmail.com>
2024-01-07 13:38:00 +0100

    adds

--- Registration-form/process.php
+++ Registration-form/process.php
@@ -1,14 +1,14 @@
 <?php
-session_start
+session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username =$_POST["username"];
     $email = $_POST["email"];
 
-    // Perform any necessary validation and database operation here
+    // Perform necessary validation and database operation here
 
     $_SESSION["username"] = $username;
 
-    //Redirect the user to the success page
+    //Redirects the user to the success page
     header("Location: success.php");
     exit();
 }
--- Registration-form/success.php
+++ Registration-form/success.php
@@ -18,7 +18,7 @@ if (isset($_SESSION["username"])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registration Successful</title>
-    <link rel="stylesheet" href="./css/style.css">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
     <div class=container>
