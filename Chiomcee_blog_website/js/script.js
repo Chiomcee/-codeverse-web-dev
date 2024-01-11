40491d1bdcfbb00e2163059b45aa51d7342206cf
Chioma Igwe <cigwe072013@gmail.com>
2024-01-11 11:22:23 +0100

    add event

--- Chiomcee_blog_website/js/script.js
+++ Chiomcee_blog_website/js/script.js
@@ -6,5 +6,25 @@ document,addEventListener('DOMContentLoaded', () => {
         text.computedStyleMap.opacity = '0';
     });
 
-    
-}
+    setTimeout(() => {
+        welcomeTexts.forEach(text => {
+            text.computedStyleMap.opacity = "i";
+            text.computedStyleMap.transition = 'opacity is ease-in-out';
+        });
+    }, 300);   
+});
+// Smooth Scrolling for navigation Links
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection =document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop - 50,
+            behavior: 'smooth'
+        });
+    });
+});
